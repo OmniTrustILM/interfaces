28dc267379ab3f2eb7ca6fb443114ea0cf48f8d2
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-05-12 09:10:52 +0200

    Add event history and per-object event history endpoints (#661)
    * Get event history for an object
    
    * Update EventController.java
    
    * Add event history
    
    * Add notifcations flag
    
    * Resolve comments
    
    * Delete NestedPaginationRequestDto.java
    
    * Update PaginationRequestDto.java
    
    * Resolve comments from review

---
+++ src/main/java/com/czertainly/api/model/core/workflows/EventStatus.java
@@ -0,0 +1,60 @@
+package com.czertainly.api.model.core.workflows;
+
+import com.czertainly.api.exception.ValidationError;
+import com.czertainly.api.exception.ValidationException;
+import com.czertainly.api.model.common.enums.IPlatformEnum;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.Arrays;
+
+@Schema(enumAsRef = true)
+public enum EventStatus implements IPlatformEnum {
+
+    IN_PROGRESS("inProgress", "In Progress", null),
+    FINISHED("finished", "Finished", null),
+    FAILED("failed", "Failed", null)
+    ;
+
+    private static final EventStatus[] VALUES;
+
+    static {
+        VALUES = values();
+    }
+
+    private final String code;
+    private final String label;
+    private final String description;
+
+    EventStatus(String code, String label, String description) {
+        this.code = code;
+        this.label = label;
+        this.description = description;
+    }
+
+    @Override
+    @JsonValue
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getLabel() {
+        return this.label;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @JsonCreator
+    public static EventStatus findByCode(String code) {
+        return Arrays.stream(VALUES)
+                .filter(k -> k.code.equals(code))
+                .findFirst()
+                .orElseThrow(() ->
+                        new ValidationException(ValidationError.create("Unknown event status code {}", code)));
+    }
+}
