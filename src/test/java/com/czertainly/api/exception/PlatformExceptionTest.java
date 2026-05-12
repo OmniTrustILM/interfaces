ce50f431872c8f4a5c589c031cf652564de20313
Ivo Raisr <ivosh@ivosh.net>
2026-05-12 13:02:26 +0200

    Remove unused import

--- src/test/java/com/czertainly/api/exception/PlatformExceptionTest.java
+++ src/test/java/com/czertainly/api/exception/PlatformExceptionTest.java
@@ -10,7 +10,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
