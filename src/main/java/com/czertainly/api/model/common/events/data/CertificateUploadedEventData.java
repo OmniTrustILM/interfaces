d4d2b4513fead0a86df2583aaa81fafc85e2c709
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-05-12 14:58:13 +0200

    Add certificate uploaded event

---
+++ src/main/java/com/czertainly/api/model/common/events/data/CertificateUploadedEventData.java
@@ -0,0 +1,19 @@
+package com.czertainly.api.model.common.events.data;
+
+import com.czertainly.api.model.client.attribute.RequestAttribute;
+import jdk.jfr.Event;
+import lombok.Data;
+
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class CertificateUploadedEventData implements EventData {
+
+    private X509Certificate certificate;
+    private String fingerprint;
+    private List<RequestAttribute> customAttributes;
+    private UUID userUuid;
+
+}
