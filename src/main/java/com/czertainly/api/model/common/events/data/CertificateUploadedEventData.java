a51b1ca17ecf40464ab301f9a953e49f9ffafac4
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-05-13 16:43:12 +0200

    Update CertificateUploadedEventData.java

--- src/main/java/com/czertainly/api/model/common/events/data/CertificateUploadedEventData.java
+++ src/main/java/com/czertainly/api/model/common/events/data/CertificateUploadedEventData.java
@@ -1,18 +1,23 @@
 package com.czertainly.api.model.common.events.data;
 
-import com.czertainly.api.model.client.attribute.RequestAttribute;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Data
-public class CertificateUploadedEventData implements EventData {
+@EqualsAndHashCode(callSuper = true)
+public class CertificateUploadedEventData extends CertificateEventData {
 
-    private X509Certificate certificate;
-    private String fingerprint;
-    private List<RequestAttribute> customAttributes;
     private UUID userUuid;
+    private List<String> customAttributes = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Certificate to be uploaded: {serialNumber='%s', subjectDn='%s', issuerDn='%s', userUuid=%s, customAttributes=%s}"
+                .formatted(getSerialNumber(), getSubjectDn(), getIssuerDn(), userUuid, customAttributes);
+    }
 
 }
