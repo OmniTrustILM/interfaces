87417479d2e0f7366ad3d50c47de324941fe96e3
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-05-14 16:13:47 +0200

    Remove custom attributes from event data

--- src/main/java/com/czertainly/api/model/common/events/data/CertificateUploadedEventData.java
+++ src/main/java/com/czertainly/api/model/common/events/data/CertificateUploadedEventData.java
@@ -1,23 +1,22 @@
 package com.czertainly.api.model.common.events.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class CertificateUploadedEventData extends CertificateEventData {
 
+    @Schema(description = "UUID of the user to assigned the certificate user", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private UUID userUuid;
-    private List<String> customAttributes = new ArrayList<>();
 
     @Override
     public String toString() {
-        return "Certificate to be uploaded: {serialNumber='%s', subjectDn='%s', issuerDn='%s', userUuid=%s, customAttributes=%s}"
-                .formatted(getSerialNumber(), getSubjectDn(), getIssuerDn(), userUuid, customAttributes);
+        return "Certificate to be uploaded: {serialNumber='%s', subjectDn='%s', issuerDn='%s', userUuid=%s}"
+                .formatted(getSerialNumber(), getSubjectDn(), getIssuerDn(), userUuid);
     }
 
 }
