cd2e444fe909ad68da098f471a04cfe8f97f95cd
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-05-18 09:53:28 +0200

    Update CertificateUploadedEventData.java

--- src/main/java/com/czertainly/api/model/common/events/data/CertificateUploadedEventData.java
+++ src/main/java/com/czertainly/api/model/common/events/data/CertificateUploadedEventData.java
@@ -10,12 +10,12 @@ import java.util.UUID;
 @EqualsAndHashCode(callSuper = true)
 public class CertificateUploadedEventData extends CertificateEventData {
 
-    @Schema(description = "UUID of the user to assigned the certificate user", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Schema(description = "UUID of the certificate user", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private UUID userUuid;
 
     @Override
     public String toString() {
-        return "Certificate to be uploaded: {serialNumber='%s', subjectDn='%s', issuerDn='%s', userUuid=%s}"
+        return "Certificate uploaded: {serialNumber='%s', subjectDn='%s', issuerDn='%s', userUuid=%s}"
                 .formatted(getSerialNumber(), getSubjectDn(), getIssuerDn(), userUuid);
     }
 
