b5827cc911e5caa7d795226da9c587e9cc37fbbc
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-05-25 08:29:36 +0200

    Update UpdateTriggerRequestDto.java

--- src/main/java/com/czertainly/api/model/core/workflows/UpdateTriggerRequestDto.java
+++ src/main/java/com/czertainly/api/model/core/workflows/UpdateTriggerRequestDto.java
@@ -4,8 +4,8 @@ import com.czertainly.api.model.core.auth.Resource;
 import com.czertainly.api.model.core.other.ResourceEvent;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +35,7 @@ public class UpdateTriggerRequestDto {
             description = "Resource associated with the trigger",
             requiredMode = Schema.RequiredMode.REQUIRED
     )
-    @NonNull
+    @NotNull
     private Resource resource;
 
     @Schema(
