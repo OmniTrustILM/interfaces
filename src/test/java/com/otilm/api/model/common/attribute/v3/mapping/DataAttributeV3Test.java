12a046ef4edfcd9a2f9247a9713195b887a53d4b
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-06-25 15:25:38 +0200

    Serialize DataAttributeV3 fieldMapping/valueSource

--- src/test/java/com/otilm/api/model/common/attribute/v3/mapping/DataAttributeV3Test.java
+++ src/test/java/com/otilm/api/model/common/attribute/v3/mapping/DataAttributeV3Test.java
@@ -1,11 +1,15 @@
 package com.otilm.api.model.common.attribute.v3.mapping;
 
+import com.otilm.api.model.common.attribute.common.BaseAttribute;
+import com.otilm.api.model.common.attribute.common.AttributeType;
 import com.otilm.api.model.common.attribute.common.content.AttributeContentType;
 import com.otilm.api.model.common.attribute.v3.DataAttributeV3;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,7 +41,7 @@ class DataAttributeV3Test {
     }
 
     @Test
-    void dataAttributeV3_nullFieldMappingAndValueSource_omittedFromJson() throws Exception {
+    void dataAttributeV3_nullFieldMappingAndValueSource_omittedFromJson() {
         DataAttributeV3 attr = new DataAttributeV3();
         attr.setName("test");
         attr.setContentType(AttributeContentType.STRING);
@@ -46,6 +50,32 @@ class DataAttributeV3Test {
 
         assertFalse(json.has("fieldMapping"));
         assertFalse(json.has("valueSource"));
+    }
+
+    @Test
+    void fieldMapping_survivesBaseAttributeSerializerRoundTrip() throws Exception {
+        RdnMappedField rdnField = new RdnMappedField();
+        rdnField.setFieldType(FieldType.RDN);
+        rdnField.setRdn("CN");
+
+        FieldMapping fm = new FieldMapping();
+        fm.setObjectType(ObjectType.X509_CERTIFICATE);
+        fm.setFields(List.of(rdnField));
+
+        DataAttributeV3 attr = new DataAttributeV3();
+        attr.setUuid("test-uuid");
+        attr.setName("commonName");
+        attr.setType(AttributeType.DATA);
+        attr.setContentType(AttributeContentType.STRING);
+        attr.setFieldMapping(fm);
+
+        String json = mapper.writerFor(BaseAttribute.class).writeValueAsString(attr);
+        DataAttributeV3 result = (DataAttributeV3) mapper.readValue(json, BaseAttribute.class);
+
+        assertNotNull(result.getFieldMapping());
+        assertEquals(1, result.getFieldMapping().getFields().size());
+        assertEquals(FieldType.RDN, result.getFieldMapping().getFields().get(0).getFieldType());
+        assertEquals("CN", ((RdnMappedField) result.getFieldMapping().getFields().get(0)).getRdn());
     }
 
 }
