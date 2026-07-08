ec8b5002bbba9d33a2377317eb654b23e2c80141
Ivo Raisr <ivosh@ivosh.net>
2026-07-08 22:38:49 +0200

    Validate certificateUuids on the compliance-check request (#760)

--- src/main/java/com/otilm/api/model/client/certificate/CertificateComplianceCheckDto.java
+++ src/main/java/com/otilm/api/model/client/certificate/CertificateComplianceCheckDto.java
@@ -1,11 +1,13 @@
 package com.otilm.api.model.client.certificate;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public class CertificateComplianceCheckDto {
-    @Schema(description = "List of UUIDs of the Certificates")
+    @NotEmpty
+    @Schema(description = "List of UUIDs of the Certificates", requiredMode = Schema.RequiredMode.REQUIRED)
     private List<String> certificateUuids;
 
     public List<String> getCertificateUuids() {
