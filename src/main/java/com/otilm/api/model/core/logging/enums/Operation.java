e6647616c412ee15102d5a9585fcd2e7c3e6edbd
Lukas Najman <lukas.najman@cloudfield.cz>
2026-09-15 14:54:16 +0200

    Fix operation naming
    Signed-off-by: Lukas Najman <lukas.najman@cloudfield.cz>

--- src/main/java/com/otilm/api/model/core/logging/enums/Operation.java
+++ src/main/java/com/otilm/api/model/core/logging/enums/Operation.java
@@ -66,7 +66,7 @@ public enum Operation implements IPlatformEnum {
     DESTROY("destroy", "Destroy"),
     LIST_KEY_USAGES("listKeyUsages", "List key usages"),
     UPDATE_KEY_USAGE("updateKeyUsage", "Update key usage"),
-    LIST_KEY_CREATION_TYPES("listKeyCreationTypes", "List key creation types"),
+    LIST_KEY_REQUEST_TYPES("listKeyRequestTypes", "List key request types"),
     ENCRYPT("encrypt", "Encrypt"),
     DECRYPT("decrypt", "Decrypt"),
     SIGN("sign", "Sign"),
